10e566cc634c5f958fa13b199b4a5ce5907951b0
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2025-10-08 15:41:17 +0200

    add changes to performance metrics

--- code/metric/performance_metric_evaluation.py
+++ code/metric/performance_metric_evaluation.py
@@ -48,10 +48,12 @@ def parse_args(argv=None) -> argparse.Namespace:
     # Data dirs
     parser.add_argument("--clean-data-dir",  type=dir_path, required=True,
                         help="Base directory for clean data (wav files)")
-    parser.add_argument("--output-data-dir", type=dir_path_allow_new,     required=True,
+    parser.add_argument("--output-data-dir", type=dir_path_allow_new,
                         help="Base directory for output data (M-channel wav files)")
 
     parser.add_argument("--fs", type=int, default=16000, help="Sample Rate (default 16000)")
+
+    # Txt file
 
     # Debug
     parser.add_argument("--debug-data-dir",  type=dir_path_allow_new, help="Output debug data to this directory")
@@ -95,10 +97,16 @@ class Config:
 
 def make_config(args: argparse.Namespace) -> Config:
     level = logging.WARNING - 10*min(args.verbose, 2)
+
+    if args.output_data_dir is not None:
+        output_dir = args.output_data_dir.expanduser().resolve()
+    else:
+        output_dir = None
+
     return Config(
         clean_data_dir=args.clean_data_dir,
         debug_data_dir=args.debug_data_dir,
-        output_data_dir=args.output_data_dir.expanduser().resolve(),
+        output_data_dir=output_dir,
         scenario_file=args.scenario_file,
         scenario_dir=args.scenario_dir,
         distant_file=args.distant_file,
@@ -112,15 +120,16 @@ def make_config(args: argparse.Namespace) -> Config:
 # ------------- app logic ----------------
 def run(cfg: Config) -> int:
     # Ensure output dirs exist
-    os.makedirs(cfg.output_data_dir, exist_ok=True)
+    if cfg.output_data_dir is not None:
+        logging.info("Will write outputs to .txt files")
+        os.makedirs(cfg.output_data_dir, exist_ok=True)
+
     if cfg.debug_data_dir is not None:
+        logging.info("Will write intermediate outputs to debug files")
         os.makedirs(cfg.debug_data_dir, exist_ok=True)
 
     logging.basicConfig(level=cfg.log_level,
                         format="[%(asctime)s %(levelname)s] %(message)s")
-
-    if cfg.debug_data_dir is not None:
-        logging.info("Will write intermediate outputs to debug files")
 
     #we assume that since only one scenario it is the same for distant and enhanced
     if cfg.scenario_file:
@@ -247,33 +256,77 @@ def process_one_scenario(cfg: Config, scenario: Path):
     logging.info(f'Processing E-STOI for {distant_path=} and {enhanced_path=}')
     estoi_score_distant  = stoi(clean_distant_mono, distant_mono, cfg.fs, extended=True)
     estoi_score_enhanced = stoi(clean_enhanced_mono, enhanced_mono, cfg.fs, extended=True)
-    print(f'Distant E-STOI score is: {estoi_score_distant}')
-    print(f'Enhance E-STOI score is: {estoi_score_enhanced}')
 
     # SRMR metric (range is (0 to 20))
     logging.info(f'Processing SRMR for {distant_path=} and {enhanced_path=}')
     srmr_score_distant, _  = srmr(distant_mono, cfg.fs, fast=True)  # We tried with mono and wav, they give same result. Also try with stereo 
     srmr_score_enhanced, _ = srmr(enhanced_mono, cfg.fs, fast=True)
-    print(f'Distant SRMR score is: {srmr_score_distant}')
-    print(f'Enhance SRMR score is: {srmr_score_enhanced}')
 
-    # PESQ metric (range is (-0.5 to 4.64) for 16kHz)
-    logging.info(f'Processing PESQ {distant_path=} and {enhanced_path=}')
-    pesq_score_distant  = pesq(cfg.fs, clean_distant_mono, distant_mono, 'wb')    # 'wb (16 kHz)' 'nb (8 kHz)'
-    pesq_score_enhanced = pesq(cfg.fs, clean_enhanced_mono, enhanced_mono, 'wb')    
-    print(f'Distant PESQ score is: {pesq_score_distant}')
-    print(f'Enhance PESQ score is: {pesq_score_enhanced}')
+    if cfg.fs == 16000:
+        # PESQ metric (range is (-0.5 to 4.64) for 16kHz)
+        logging.info(f'Processing PESQ {distant_path=} and {enhanced_path=}')
+        pesq_score_distant  = pesq(cfg.fs, clean_distant_mono, distant_mono, 'wb')    # 'wb (16 kHz)' 'nb (8 kHz)'
+        pesq_score_enhanced = pesq(cfg.fs, clean_enhanced_mono, enhanced_mono, 'wb')    
+    
+        # convert int16 to float (DNSMOS is in range (-1,1))
+        distant_mono_float = distant_mono.astype(np.float32) / 32768.0
+        enhanced_mono_float = enhanced_mono.astype(np.float32) / 32768.0    
 
-    # convert int16 to float (DNSMOS is in range (-1,1))
-    distant_mono_float = distant_mono.astype(np.float32) / 32768.0
-    enhanced_mono_float = enhanced_mono.astype(np.float32) / 32768.0    
+        # DNSMOS metric (range is (1 to 5)) (only 8kHz and 16kHz sample rate work for DNSMOS)
+        logging.info(f'Processing DNSMOS {distant_path=} and {enhanced_path=}')
+        dnsmos_score_distant  = dnsmos.run(distant_mono_float, cfg.fs)
+        dnsmos_score_enhanced = dnsmos.run(enhanced_mono_float, cfg.fs)
+    
+    else:
+        logging.warning(f'PESQ and DNSMOS needs 16 kHz sample rate. Therefore, they are set to 0')
+        pesq_score_distant    = 0
+        pesq_score_enhanced   = 0
+        dnsmos_score_distant  = {"ovrl_mos": 0, "sig_mos": 0, "bak_mos": 0}
+        dnsmos_score_enhanced = {"ovrl_mos": 0, "sig_mos": 0, "bak_mos": 0}
 
-    # DNSMOS metric (range is (1 to 5)) (only 8kHz and 16kHz sample rate work for DNSMOS)
-    logging.info(f'Processing DNSMOS {distant_path=} and {enhanced_path=}')
-    dnsmos_score_distant  = dnsmos.run(distant_mono_float, cfg.fs)
-    dnsmos_score_enhanced = dnsmos.run(enhanced_mono_float, cfg.fs)
-    print(f'Distant DNSMOS overall score: {dnsmos_score_distant['ovrl_mos']}, speech quality score: {dnsmos_score_distant['sig_mos']}, background noise score {dnsmos_score_distant['bak_mos']}')   # p808 = P.808 mapping   
-    print(f'Enhance DNSMOS overall score: {dnsmos_score_enhanced['ovrl_mos']}, speech quality score: {dnsmos_score_enhanced['sig_mos']}, background noise score {dnsmos_score_enhanced['bak_mos']}')
+    if cfg.output_data_dir is None:
+        logging.info(f'Printing metric scores')
+        # E-STOI
+        print(f'Distant E-STOI score is: {estoi_score_distant}')
+        print(f'Enhance E-STOI score is: {estoi_score_enhanced}')
+
+        # SRMR
+        print(f'Distant SRMR score is: {srmr_score_distant}')
+        print(f'Enhance SRMR score is: {srmr_score_enhanced}')
+
+        # PESQ
+        print(f'Distant PESQ score is: {pesq_score_distant}')
+        print(f'Enhance PESQ score is: {pesq_score_enhanced}')
+
+        # DNSMOS
+        print(f'Distant DNSMOS overall score: {dnsmos_score_distant['ovrl_mos']}, speech quality score: {dnsmos_score_distant['sig_mos']}, background noise score {dnsmos_score_distant['bak_mos']}')   # p808 = P.808 mapping   
+        print(f'Enhance DNSMOS overall score: {dnsmos_score_enhanced['ovrl_mos']}, speech quality score: {dnsmos_score_enhanced['sig_mos']}, background noise score {dnsmos_score_enhanced['bak_mos']}')
+    
+    else:
+        logging.info(f'Making .txt file for metric scores')
+
+        # Prepare metric table
+        metrics = [
+        ("E-STOI", estoi_score_distant, estoi_score_enhanced),
+        ("SRMR", srmr_score_distant, srmr_score_enhanced),
+        ("PESQ", pesq_score_distant, pesq_score_enhanced),
+        ("DNSMOS_ovrl", dnsmos_score_distant['ovrl_mos'], dnsmos_score_enhanced['ovrl_mos']),
+        ("DNSMOS_sig",  dnsmos_score_distant['sig_mos'],  dnsmos_score_enhanced['sig_mos']),
+        ("DNSMOS_bak",  dnsmos_score_distant['bak_mos'],  dnsmos_score_enhanced['bak_mos']),
+        ]
+
+        # File path: use scenario name as filename
+        out_file = cfg.output_data_dir / f"{scenario.stem}_metrics.txt"
+
+        # Write to file
+        with open(out_file, "w") as f:
+            f.write(f"{'Metric':<15}{'Distant':>12}{'Enhanced':>12}\n")
+            f.write("="*39 + "\n")
+            for name, distant, enhanced in metrics:
+                f.write(f"{name:<15}{distant:>12.4f}{enhanced:>12.4f}\n")
+
+        logging.info(f"Saved metric scores to {out_file}")
+
 
     ''' Playing Sound
     sd.play(clean_distant_wav, cfg.fs)
