df6cff0af773838067318b58fdc6b7b1672628d7
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2025-10-14 16:48:28 +0200

    fix spatial diffuse coherence matrix (Gamma)

--- code/algorithms/python_isclp_kalman/spatial/calc_diffcoherence.py
+++ code/algorithms/python_isclp_kalman/spatial/calc_diffcoherence.py
@@ -20,23 +20,24 @@ import numpy as np
 from scipy.special import j0 
 
 def calc_diffcoherence(micPos, N_STFT, fs, c, reg, type='spherical'):
-    
-    # frequency vector
     N_STFT_half = N_STFT // 2 + 1
-    f = np.linspace(0, fs/2, N_STFT_half)
-    
+    f = np.linspace(0.0, fs/2.0, N_STFT_half)          # match MATLAB
     M = micPos.shape[0]
-    Gamma = (1 + reg) * np.ones((N_STFT_half, 1, M, M))
+
+    # Start with (1+reg) on the diagonal and elsewhere (like MATLAB)
+    Gamma = (1.0 + reg) * np.ones((N_STFT_half, 1, M, M), dtype=float)
 
     for m_out in range(M-1):
         for m_in in range(m_out+1, M):
-            d = np.linalg.norm(micPos[m_out, :] - micPos[m_in, :])
+            d = np.linalg.norm(micPos[m_out] - micPos[m_in])
             if type == 'spherical':
-                Gamma[:, 0, m_out, m_in] = np.sinc(2*f*d/c)
+                # MATLAB’s sinc is normalized; numpy.sinc matches that: sin(pi x)/(pi x)
+                val = np.sinc(2.0 * f * d / c)
             elif type == 'cylindrical':
-                Gamma[:, 0, m_out, m_in] = j0(2*np.pi*f*d/c)
+                val = j0(2.0 * np.pi * f * d / c)
             else:
-                raise ValueError("type must be 'spherical' or 'clyndrical'")
-            Gamma[:, 0, m_out, m_in] = Gamma[:, 0, m_out, m_in]
-    
+                raise ValueError("type must be 'spherical' or 'cylindrical'")
+            Gamma[:, 0, m_out, m_in] = val
+            Gamma[:, 0, m_in, m_out] = val   # <-- mirror to make it symmetric!
+
     return Gamma
